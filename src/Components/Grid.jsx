13de7d1a152f75be611cc2ea14eaa2935dd9ee2a
Asmaa Ramadan <rasmaa99@yahoo.com>
2021-08-05 15:37:17 +0200

    Update Grid.jsx

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -10,59 +10,61 @@ class Grid extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            tiles: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+            tiles: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+        
         }
+        
+       
         this.setChanged = this.setChanged.bind(this);
     }
-    
-    componentDidMount(){
-      this.refresh()
-    }
-   
-    refresh(){
-        this.setState({tiles : Utils.shuffle(this.state.tiles)})
+
+    componentDidMount() {
+        this.refresh()
     }
 
-    setChanged( t) {
-        this.setState({ tiles : t });
-      }
-  
+    refresh() {
+        this.setState({ tiles: Utils.shuffle(this.state.tiles) })
+    }
+
+    setChanged(t) {
+        this.setState({ tiles: t });
+    }
+
 
     render() {
-        let index=0
+
 
         return (
             <div className='container'>
-           <div className=' containGrid'>
-           <div>
-               <h1>Welcome to our game</h1>
-           </div>          
-            <div className='Grid'>
-            <div className='buttonsPlace'>
-            <button className='buttons' onClick={()=>this.refresh()}  > Shuffle </button>
-
-            <div><h1>
-                </h1></div>
-            <button className='buttons' onClick={()=>this.refresh()}  > order </button>
-            </div>
-            <div></div>
-            <div></div>
-            <div></div>
-
+                <div className=' containGrid'>
+                    <div>
+                        <h1>Welcome to our game</h1>
+                    </div>
+                    <div className='Grid'>
+                        <div className='buttonsPlace'>
+                            <button className='buttons' onClick={() => this.refresh()}  > Shuffle </button>
+                            <button className='buttons' onClick={() => this.refresh()}  > order </button>
+                        </div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+    
 
                         {
-                               this.state.tiles.map(
-                                tile =>  
-                                <Tile setChanged = {this.setChanged} grid={this.state.tiles}
-                                 neighbours = {Utils.getNeighbours(index)} index={index++} number={tile.toString()} id={tile.toString} >{console.log(tile.toString())}</Tile> 
-                                )
-                      }
-                      
+                            this.state.tiles.map(
+                                (tile, i) =>
+                                    <Tile grid= {this.state.tiles} setChanged = {this.setChanged} neighbours = {Utils.getNeighbours(i)} index={i} number={tile}/>
+
+                            
+                            )}
+
+                    </div>
+                </div>
             </div>
-            </div>
-            </div>
-            
+
 
         )
-    }
+    
+   }     
+             
 } export default Grid
