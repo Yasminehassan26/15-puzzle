637e2097fa6a72be2bee2719f476303e18daf084
Asmaa Ramadan <rasmaa99@yahoo.com>
2021-08-05 15:37:57 +0200

    Update Tile.jsx

--- src/Components/Tile.jsx
+++ src/Components/Tile.jsx
@@ -1,55 +1,62 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import Utils from '../Utils';
-import Grid from './Grid';
 import './Tilestyle.css';
 
 
 
-class Tile extends Component{
+class Tile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            number: '',
-            neighbours : {'L' : -1 ,'R' : -1 ,'U' :-1 ,'D' :-1}
+            number: props.number,
+            neighbours: [ -1, -1, -1,  -1 ]
         }
-        this.handleChange=this.handleChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
-   
-    render() {
-        let showInfo=true
-        if(this.props.number==16) {Utils.emptyTile=this.props.index
-           showInfo = false
 
-        }
-        return(
-            <span style={{display: showInfo ? "block" : "none" }} onClick={this.handleChange} className="Tile">
-                {this.props.number}   
+    render() {
+        console.log(this.props.index);
+        // let showInfo = true
+        // if (this.props.number === 16) {
+        //     Utils.emptyTile = this.props.index
+        //     showInfo = false
+
+        // }
+        return (
+            <span style={{ display: "block" }} onClick={this.handleChange} className="Tile">
+                {this.props.number!==16 && this.props.number}
             </span>
         )
-        
+
     }
-    refresh()
-    {
-        this.render()
-    }
-    handleChange(){
-       // Utils.isValidMove();
-       console.log(Utils.getNeighbours(this.props.index))
+    checkValidMove(){
+        let neighbours = Utils.getNeighbours(this.props.index)
         let a = this.props.grid
-        a[6]=-111
-        console.log(a)
-      /* console.log(this.props.index)
-       console.log(this.tiles)*/
-      // console.log(`neighnbourr is ${this.props.neighbours['R']}`)
-     /*  if (16===this.props.tiles[this.props.neighbours['R']]){
-       var b = this.props.tiles[this.props.neighbours['R']];
-       this.props.tiles[this.props.neighbours['R']] =  this.props.tiles[this.props.index-1];
-       this.props.tiles[this.props.index-1] = b;
-       }*/
 
-       this.props.setChanged(this.props.tiles)
+        for (let i =0 ;i<4;i++)
+        {
+            if(neighbours[i]!=-1 && a[neighbours[i]]==16) return neighbours[i]
+        }
+        return -1
+    }
+    handleChange() {
+        let neighbours = Utils.getNeighbours(this.props.index)
     
-
+        
+        console.log("Neighbours", neighbours)
+        let a = this.props.grid
+        let toIndex = this.checkValidMove()
+        console.log( toIndex)
+        console.log(this.props.index)
+        if(toIndex!== -1)
+        {
+            var b = a[this.props.index];
+            a[this.props.index] = a[toIndex];
+            a[toIndex] = b;
+            [a[this.props.index], a[toIndex]] = [a[toIndex], a[this.props.index]]
+        }
+        console.log("a = ", a);
+      this.props.setChanged(a)
     }
 }
 export default Tile
